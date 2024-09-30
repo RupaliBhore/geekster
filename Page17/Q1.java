bf2b96dda7be3ba3517542aef2b739394b2da9d5
RupaliBhore <121439689+RupaliBhore@users.noreply.github.com>
2024-09-30 19:48:45 +0530

    Update Q1.java

--- Page17/Q1.java
+++ Page17/Q1.java
@@ -54,7 +54,26 @@
 // 89
 // 99
 // 56
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       
+        
+        int t=sc.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt(); 
+            
+            System.out.println(x+""+y);
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
 // import java.util.*;
 
 // public class Q1 {
