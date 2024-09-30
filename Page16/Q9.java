31bc305dbad7618f7a6c20141715d2a82f0f5b84
RupaliBhore <121439689+RupaliBhore@users.noreply.github.com>
2024-09-30 19:38:21 +0530

    Update Q9.java

--- Page16/Q9.java
+++ Page16/Q9.java
@@ -29,30 +29,57 @@
 
 // 7+10>5 So,triangle is possible
 
+import java.io.*;
 import java.util.*;
 
-public class Q9 {
+public class Solution Q9 {
+    
+    static boolean validTra(int a, int b, int c)
+    {
+        if(a+b>c)
+        {
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Reading inputs A, B, and C
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        int C = scanner.nextInt();
-
-        // Checking if triangle is valid
-        boolean isValidTriangle = isTriangleValid(A, B, C);
-
-        // Printing the result
-        System.out.println(isValidTriangle);
-
-        scanner.close();
-    }
-
-    // Function to check if three sides can form a valid triangle
-    public static boolean isTriangleValid(int A, int B, int C) {
-        // Check triangle inequality theorem
-        return (A + B > C) && (A + C > B) && (B + C > A);
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        
+       boolean ans= validTra(a,b,c);
+           System.out.println(ans);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
+
+
+// import java.util.*;
+
+// public class Q9 {
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         // Reading inputs A, B, and C
+//         int A = scanner.nextInt();
+//         int B = scanner.nextInt();
+//         int C = scanner.nextInt();
+
+//         // Checking if triangle is valid
+//         boolean isValidTriangle = isTriangleValid(A, B, C);
+
+//         // Printing the result
+//         System.out.println(isValidTriangle);
+
+//         scanner.close();
+//     }
+
+//     // Function to check if three sides can form a valid triangle
+//     public static boolean isTriangleValid(int A, int B, int C) {
+//         // Check triangle inequality theorem
+//         return (A + B > C) && (A + C > B) && (B + C > A);
+//     }
+// }
